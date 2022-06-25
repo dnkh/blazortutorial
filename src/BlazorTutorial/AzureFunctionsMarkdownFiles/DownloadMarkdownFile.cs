e8205a46969ee39c21d0aca626f855abcca9d1f7
Julian Weidmann <jweidmann@macc02dq0qsml86.fritz.box>
2022-06-25 16:00:25 +0200

    First draft

---
+++ src/BlazorTutorial/AzureFunctionsMarkdownFiles/DownloadMarkdownFile.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace AzureFunctionsMarkdownFiles
+{
+    public static class DownloadMarkdownFile
+    {
+        [FunctionName("DownloadMarkdownFile")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "DownloadMarkdownFile/{blobName}")] HttpRequest req,
+            string blobName)
+        {
+            StorageCredentials storageCredentials = new ("Storage", "CamEKgqVaylmQ.....ow2VHlyCww=="); // adjust
+            CloudStorageAccount storageAccount = new (storageCredentials, true);
+            CloudBlobContainer container = storageAccount.CreateCloudBlobClient().GetContainerReference("MyBlobContainer");
+            CloudBlockBlob block = container.GetBlockBlobReference(blobName);
+            Stream blobStream = await block.OpenReadAsync();
+
+            HttpResponseMessage message = new (HttpStatusCode.OK);
+            message.Content = new StreamContent(blobStream);
+            message.Content.Headers.ContentLength = block.Properties.Length;
+            message.StatusCode = HttpStatusCode.OK;
+            message.Content.Headers.ContentType = new MediaTypeHeaderValue(block.Properties.ContentType);
+            message.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = $"CopyOf_{block.Name}",
+                Size = block.Properties.Length
+            };
+            return message;
+        }
+    }
+}
+
---
+++ src/BlazorTutorial/AzureFunctionsMarkdownFiles/UploadMarkdownFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+
+namespace AzureFunctionsMarkdownFiles
+{
+    public static class UploadMarkdownFile
+    {
+        [FunctionName("UploadMarkdownFile")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req)
+        {
+            string connection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string containerName = Environment.GetEnvironmentVariable("ContainerName");
+
+            var file = req.Form.Files["File"];
+            Stream myBlob = file.OpenReadStream();
+            var blobClient = new BlobContainerClient(connection, containerName);
+            var blob = blobClient.GetBlobClient(file.FileName);
+            await blob.UploadAsync(myBlob);
+
+            return new OkObjectResult("File uploaded successfully.");
+        }
+    }
+}
+
