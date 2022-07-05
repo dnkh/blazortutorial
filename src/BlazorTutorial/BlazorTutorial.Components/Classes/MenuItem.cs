6c4582b17aedb4b11441edb7e57507c365a40b11
Marcus Kempf <marcus.kempf@bridging-it.de>
2022-07-05 21:36:00 +0200

    Added dynamic menu.

---
+++ src/BlazorTutorial/BlazorTutorial.Components/Classes/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorTutorial.Components.Classes
+{
+    public class MenuItem
+    {
+        public MenuItem()
+        {
+            Children = new List<MenuItem>();
+        }
+
+        public string Link { get; set; }
+
+        public string Text { get; set; }
+
+        public string IconName { get; set; }
+
+        public List<MenuItem> Children { get; set; }
+    }
+}
